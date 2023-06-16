87745716ad6ba201892f062bb96bb932246e07ef
Jantopthegreat <fauzangriya@gmail.com>
2023-06-16 15:07:33 +0700

    bool empty

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -9,33 +9,39 @@ private:
 	int top;
 
 public:
-//constructor
+	//constructor
 	StackArray() {
 		top = -1;
 	}
 
 
-int push(int element) {
-	if (top == 4) {
-		cout << "Number of Data exceed the list" << endl;
-		return 0;
+	int push(int element) {
+		if (top == 4) {
+			cout << "Number of Data exceed the list" << endl;
+			return 0;
 
+		}
+
+		top++;
+		stack_array[top] = element;
+		cout << endl;
+		cout << element << "ditambahkan(pushed)" << endl;
+		return element;
 	}
 
-	top++;
-	stack_array[top] = element;
-	cout << endl;
-	cout << element << "ditambahkan(pushed)" << endl;
-	return element;
-}
-
-void pop() {
-	if (empty()) { // step 2
-		cout << "\n Stack is Empty, Cannot pop" << endl;
-		return;
+	void pop() {
+		if (empty()) { // step 2
+			cout << "\n Stack is Empty, Cannot pop" << endl;
+			return;
+		}
+		cout << "\n The Popped Element is : " << stack_array[top] << endl;
 	}
-	cout << "\n The Popped Element is : " << stack_array[top] << endl;
-}
+
+	bool empty() {
+		return (top == -1);
+	}
+
+	
 
 
 
