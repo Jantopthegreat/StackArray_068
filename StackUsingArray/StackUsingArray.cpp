603a92579344df9f02a4708568f72e13c4a2cacb
Jantopthegreat <fauzangriya@gmail.com>
2023-06-16 14:49:27 +0700

    membuat class StackArray dengan constructor stack array = - 1

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class StackArray {
+private:
+	int stack_array[5];
+	int top;
+
+public:
+//constructor
+	StackArray() {
+		top = -1;
+	}
+};
